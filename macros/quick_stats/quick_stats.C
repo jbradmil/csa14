7835df0756afa4924dcff5ed2ccb3fd855b13d6f
Jack Bradmiller-Feld <jbradmil@cern.ch>
2014-08-04 15:42:28 +0200

    Save compare samples macro inputs.

--- macros/quick_stats/quick_stats.C
+++ macros/quick_stats/quick_stats.C
@@ -12,9 +12,11 @@ void quick_stats(TString files) {
   TChain* chain = new TChain("reduced_tree");
   chain->Add(files);
 
-  int nTotal(chain->GetEntries()), n_true_mu(chain->GetEntries("num_gen_muons==1")), n_lost_mu(chain->GetEntries("num_gen_muons==1&&num_reco_veto_muons==0"));
+  int nTotal(chain->GetEntries()), n_true_mu(chain->GetEntries("num_gen_muons==1")), n_lost_mu(chain->GetEntries("num_gen_muons==1&&muon_veto<1"));
+  // n_lost_mu = chain->GetEntries("num_gen_muons==1&&num_reco_veto_muons==0");
   TCut matched("num_gen_muons==1&&muon_reco_match>=0");
-  int n_no_match(chain->GetEntries("num_gen_muons==1"+!matched)), n_fail_iso(chain->GetEntries(matched+"muon_relIso>0.2")), n_fail_pt(chain->GetEntries(matched+"muon_pt<10")), n_fail_both(chain->GetEntries(matched+"muon_relIso>0.2&&muon_pt<10"));
+  int n_no_match(chain->GetEntries("num_gen_muons==1"+!matched)), n_fail_iso(chain->GetEntries(matched+"muon_relIso>0.2")), n_fail_pt(chain->GetEntries(matched+"muon_pt<10"));
+  int n_fail_both(chain->GetEntries(matched+"muon_relIso>0.2&&muon_pt<10")), n_fail_either(chain->GetEntries(matched+"muon_relIso>0.2||muon_pt<10"));
 
   printf("Sample: %s\n", files.Data());
   printf("Events: %d\n", nTotal);
@@ -25,4 +27,5 @@ void quick_stats(TString files) {
   printf("Fails pt: %d (%3.1f %%)\n", n_fail_pt, 100*n_fail_pt/(float)n_lost_mu);
   printf("Fails iso: %d (%3.1f %%)\n", n_fail_iso, 100*n_fail_iso/(float)n_lost_mu);
   printf("Fails both: %d (%3.1f %%)\n", n_fail_both, 100*n_fail_both/(float)n_lost_mu);
+  printf("Fails either: %d (%3.1f %%)\n", n_fail_either, 100*n_fail_either/(float)n_lost_mu);
 }
